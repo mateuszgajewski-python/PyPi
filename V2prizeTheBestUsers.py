2ec12627be19d9dded4d7719f20cd5716ab0ce1b
Mateusz Gajewski <mateuszgajewski@gmail.com>
2021-02-13 13:11:56 +0100

    21.02.13

--- V2prizeTheBestUsers.py
+++ V2prizeTheBestUsers.py
@@ -31,16 +31,27 @@ theBestUsers = keys_with_top_values(userIdWithPoints)
 
 print(theBestUsers)
 
+def tail_of_address_ampersand(myList, key = "id"):
+    i = 1
+    wynik = ""
+    for x in myList:
+        if(len(myList) == i):
+            wynik += key + "=" + str(x)
+        else:
+            wynik += key + "=" + str(x) + "&"
+        i += 1
+        
+    return wynik
 
-#sposob 1
-r = requests.get("https://jsonplaceholder.typicode.com/users")
 
+#sposób 2
+for theBestUser in theBestUsers:
+    r = requests.get("https://jsonplaceholder.typicode.com/users/" + str(theBestUser))
+    user = r.json()
+    print("Ciasteczko dostaje użytkownik o imieniu", user["name"])
+
+#sposób 3
+r = requests.get("https://jsonplaceholder.typicode.com/users?" + tail_of_address_ampersand(theBestUsers))
 users = r.json()
-
 for user in users:
-    if user["id"] in theBestUsers:
-        print("Ciasteczko dostaje użytkownik o imieniu", user["name"])
-    
-
-            
-
+    print ("Ciasteczko dostaje użytkownik o imieniu", user["name"])
